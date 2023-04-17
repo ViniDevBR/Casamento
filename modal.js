7de971df686bb3c36f5b5d2c722e240cfe400b7e
ViniDevBR <vinigtr386@gmail.com>
2023-04-17 11:21:23 -0300

    :fire: Fim do site

--- modal.js
+++ modal.js
@@ -4,9 +4,16 @@ const closeModalMoney = document.querySelector('#closeModal')
 const modalGift = document.querySelector('#modalPresente')
 const openModalGift = document.querySelector('#openModalGift')
 const closeModalGift = document.querySelector('#closeModalGift')
+const modalWhatsapp = document.querySelector('#modalWhatsapp')
+const openModalWhatsapp = document.querySelector('#openModalWhatsapp')
+const closeModalWhatsapp = document.querySelector('#closeModalWhatsapp')
+const closeModalWhatsappByButton = document.querySelector(
+  '#closeModalWhatsappByButton'
+)
 
 openModalMoney.addEventListener('click', () => {
   modalMoney.classList.add('isModalOpen')
+  window.scrollTo({ top: 0, behavior: 'smooth' })
 })
 
 closeModalMoney.addEventListener('click', () => {
@@ -15,8 +22,22 @@ closeModalMoney.addEventListener('click', () => {
 
 openModalGift.addEventListener('click', () => {
   modalGift.classList.add('isModalOpen')
+  window.scrollTo({ top: 0, behavior: 'smooth' })
 })
 
 closeModalGift.addEventListener('click', () => {
   modalGift.classList.remove('isModalOpen')
+})
+
+openModalWhatsapp.addEventListener('click', () => {
+  modalWhatsapp.classList.add('isModalOpen')
+  window.scrollTo({ top: 0, behavior: 'smooth' })
+})
+
+closeModalWhatsapp.addEventListener('click', () => {
+  modalWhatsapp.classList.remove('isModalOpen')
+})
+
+closeModalWhatsappByButton.addEventListener('click', () => {
+  modalWhatsapp.classList.remove('isModalOpen')
 })
